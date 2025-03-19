ccd44c54a27d6362a7a7e3a304042465d12430f3
Velid-387 <velid7jelovac@gmail.com>
2025-03-20 00:21:21 +0100

    Add testimonial cards images

--- src/app/components/testimonials/testimonials.component.ts
+++ src/app/components/testimonials/testimonials.component.ts
@@ -15,42 +15,42 @@ export class TestimonialsComponent implements OnInit {
     {
       content: 'Cvijeće koje sam dobila od Huzura bilo je prelijepo! Svježe, mirisno i predivno aranžirano. Svi su me pitali odakle je.',
       name: 'Amina H.',
-      imagePath: 'assets/img/products/huzur-buket-5.png',
+      imagePath: 'assets/img/products/huzur-buket-6.jpg',
       flipped: false,
       cardFlipping: false
     },
     {
       content: 'Naručila sam aranžman za rođendan svoje majke i bila sam oduševljena. Kvaliteta cvjetova je izvanredna, a isporuka je bila točno na vrijeme.',
       name: 'Emina B.',
-      imagePath: 'assets/img/products/huzur-buket-2.png',
+      imagePath: 'assets/img/products/huzur-buket-7.jpg',
       flipped: false,
       cardFlipping: false
     },
     {
       content: 'Huzur je moj prvi izbor za sve cvjetne potrebe. Njihovi aranžmani su uvijek prekrasni i dugotrajni, a osoblje ljubazno i profesionalno.',
       name: 'Amir M.',
-      imagePath: 'assets/img/products/huzur-cvijece-1.png',
+      imagePath: 'assets/img/products/huzur-buket-11.jpg',
       flipped: false,
       cardFlipping: false
     },
     {
       content: 'Oduševljena sam uslugom i kreativnošću koju Huzur pruža. Svaki put kad naručim, dobivam nešto posebno što nadmašuje moja očekivanja.',
       name: 'Lejla S.',
-      imagePath: 'assets/img/products/huzur-buket-3.png',
+      imagePath: 'assets/img/products/huzur-buket-9.jpg',
       flipped: false,
       cardFlipping: false
     },
     {
       content: 'Naručio sam buket za godišnjicu braka i bio sam vrlo impresioniran kvalitetom i svježinom cvijeća. Definitivno ću ponovno koristiti njihove usluge.',
       name: 'Edin D.',
-      imagePath: 'assets/img/products/huzur-box-1.png',
+      imagePath: 'assets/img/products/huzur-buket-10.jpg',
       flipped: false,
       cardFlipping: false
     },
     {
       content: 'Cvijeće za moje vjenčanje bilo je savršeno! Huzur je razumio točno što sam želio i isporučio čarobne aranžmane koji su oduševili sve goste.',
       name: 'Selma K.',
-      imagePath: 'assets/img/products/huzur-kucno-cvijece-1.png',
+      imagePath: 'assets/img/products/huzur-buket-8.jpg',
       flipped: false,
       cardFlipping: false
     }
