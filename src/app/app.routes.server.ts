064344e29ed667735d18f73d15c59c9d3826ce38
Velid-387 <velid7jelovac@gmail.com>
2025-03-28 23:02:25 +0100

    Fix build issue with prerendering

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -11,7 +11,16 @@ export const serverRoutes: ServerRoute[] = [
   },
   {
     path: 'blog/:id',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      // Return an array of objects with the 'id' parameter for all blog posts
+      // This is a static approach since we can't access the service directly here
+      return [
+        { id: 'cvijece-za-prigode' },
+        { id: 'suho-cvijece' }
+        // Add all your other blog post IDs here
+      ];
+    }
   },
   {
     path: '**',
