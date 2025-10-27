0df4c491dc4ece2619ddfff07a231c7911cd8b38
velid-387 <velid7jelovac@gmail.com>
2025-10-27 19:54:14 +0100

    Implement image optimization

--- src/app/components/testimonials/testimonials.component.ts
+++ src/app/components/testimonials/testimonials.component.ts
@@ -15,42 +15,42 @@ export class TestimonialsComponent implements OnInit {
     {
       content: 'Hvala vama što ste nam ukrasili ovaj dan. Baš mi je bilo lagano komunicirati s vama, još jedna od mnogih dokaza u nizu da su žene sjajne u poslu!',
       name: '#huzurljudi',
-      imagePath: 'assets/img/testimonials/recenzija-1.jpg',
+      imagePath: 'assets/img/testimonials/optimized/recenzija-1.webp',
       flipped: false,
       cardFlipping: false
     },
     {
       content: 'Mali buket nam se baš, baš, baš svidio, očarane smo, i veliki nam se svidio, ali mali je posebnost, baš se prijateljica obradovala. Mašallah.',
       name: '#huzurljudi',
-      imagePath: 'assets/img/testimonials/recenzija-2.jpg',
+      imagePath: 'assets/img/testimonials/optimized/recenzija-2.webp',
       flipped: false,
       cardFlipping: false
     },
     {
       content: 'Želim vam se iskreno zahvaliti na predivnom buketu! Potpuno je nadmašio sva moja očekivanja. Kad sam rekla da želim da cvijeće priča, zaista ste to ostvarile! Svaka čast na vašoj kreativnosti i trudu. Vaš Huzur diplomant.',
       name: '#huzurljudi',
-      imagePath: 'assets/img/testimonials/recenzija-3.jpg',
+      imagePath: 'assets/img/testimonials/optimized/recenzija-3.webp',
       flipped: false,
       cardFlipping: false
     },
     {
       content: 'Pišem vam samo da vas pohvalim. Vaš trud i rad. Hvala vam što postojite, što svakodnevno svojim postojanjem i radom dijelite toliko ljepote i pozitivizma. Uživam gledati kako rastete. Allah vam bereket povećao.',
       name: '#huzurljudi',
-      imagePath: 'assets/img/testimonials/recenzija-4.jpg',
+      imagePath: 'assets/img/testimonials/optimized/recenzija-4.webp',
       flipped: false,
       cardFlipping: false
     },
     {
       content: 'Wow, koje ste vi osvježenje u ovom gradu.',
       name: '#huzurljudi',
-      imagePath: 'assets/img/testimonials/recenzija-5.jpg',
+      imagePath: 'assets/img/testimonials/optimized/recenzija-5.webp',
       flipped: false,
       cardFlipping: false
     },
     {
       content: 'Tamo nećete kupiti samo cvijet, tamo ćete dotaknuti svoj svijet. Huzur mir i spokoj znači, a valjda cvijeće tako i zrači.',
       name: '#huzurljudi',
-      imagePath: 'assets/img/testimonials/recenzija-6.jpg',
+      imagePath: 'assets/img/testimonials/optimized/recenzija-6.webp',
       flipped: false,
       cardFlipping: false
     }
